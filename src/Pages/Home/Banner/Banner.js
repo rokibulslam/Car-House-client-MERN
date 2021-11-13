1d805d8015d709772db315c6bc98a86d59574fbf
rokibulslam <rokibulslam6@gmail.com>
2021-11-13 06:59:16 +0600

    banner

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,47 +1,57 @@
-import { Container, Typography } from '@mui/material';
+import { Container, Grid, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import React from 'react';
-import bg from '../../../images/cycle3.png'
+
+import './Banner.css'
 
 
 const Banner = () => {
-    const bannerBg = {
-        background: `url(${bg})`,
+    // const bannerBg = {
+    //     background: `url(${bg})`,
         
-    }
+    // }
 
     return (
-      <Box style={bannerBg} sx={{ flexGrow: 1, p: 5 }}>
-        <Typography
-          style={{ color: "white" }}
-          sx={{ fontWeight: 600 }}
-          variant="h1"
-          gutterBottom
-          component="div"
-        >
-          The B.I.Cycle Shop
-        </Typography>
-        <Typography
-          style={{ color: "white" }}
-          sx={{ fontWeight: 600 }}
-          variant="h1"
-          gutterBottom
-          component="div"
-        >
-          Experience
-        </Typography>
-        <Typography
-          style={{ color: "white" }}
-          sx={{ fontWeight: 600 }}
-          variant="h3"
-          gutterBottom
-          component="div"
-        >
-          We offer a supportive and personal interaction with our <br />
-          customers, and strive to provide the most complete and <br />
-          positive customer experience available from a bike shop.
-        </Typography>
-      </Box>
+      <div className="banner">
+        <Box style={{paddingTop: '100px', paddingBottom: '300px'}} sx={{}}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={6}>
+              <Container>
+                <Typography
+                  style={{ color: "white" }}
+                  sx={{ fontWeight: 400 }}
+                  variant="h1"
+                  gutterBottom
+                  component="div"
+                >
+                  The B.I.Cycle Shop
+                </Typography>
+                <Typography
+                  style={{ color: "white" }}
+                  sx={{ fontWeight: 400 }}
+                  variant="h1"
+                  gutterBottom
+                  component="div"
+                >
+                  Experience
+                </Typography>
+                <Typography
+                  style={{ color: "white" }}
+                  sx={{ fontWeight: 400 }}
+                  variant="h3"
+                  gutterBottom
+                  component="div"
+                >
+                  We offer a supportive and personal interaction with our
+                  customers, and strive to provide the most complete and
+                  positive customer experience available from a bike shop.
+                </Typography>
+              </Container>
+            </Grid>
+            <Grid item xs={4}></Grid>
+          </Grid>
+        </Box>
+      </div>
     );
 };
 
